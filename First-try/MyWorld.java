d2cca2a98a82102db1f2c5ba8fef2bb24d8c2302
Kim0516 <47321823+Kim0516@users.noreply.github.com>
2019-06-13 21:26:31 +0200

    Add files via upload
    Platforms now work and the game stops if the timer ends.
    I also added platforms for 30 seconds of gameplay.

--- First-try/MyWorld.java
+++ First-try/MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    private int timer = 10000;
+    private int timer = 3000;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -18,21 +18,52 @@ public class MyWorld extends World
         // Create a new world with a cell size of 1x1 pixels.
         super(720, 720, 1, false);
         addObject(new Hero(),360, 610);
+        addObject(new Platform(),500,550);
+        addObject(new Platform(),720,500);
+        addObject(new Platform(),950,260);
+        addObject(new Platform(),1200,400);
+        addObject(new Platform(),1400,234);
+        addObject(new Platform(),1300,550);
+        addObject(new Platform(),1540,470);
+        addObject(new Platform(),2134,552);
+        addObject(new Platform(),2268,316);
+        addObject(new Platform(),2503,316);
+        addObject(new Platform(),2903,556);
+        addObject(new Platform(),3103,373);
         update();
     }
     int baseX = 0;
     // 'bgi' stands for BackGroundImage
     GreenfootImage bgi = new GreenfootImage("Flat Night 2 BG.png"); 
     int bgiWidth = bgi.getWidth();
+
+    public int WorldWidth = 0;
     public void act()
     {
         baseX = (baseX + bgiWidth - 1) % bgiWidth;
         update();
 
         timer--;
-        showText(""+timer, 720, 10);
+        if(timer > 1000)
+        {
+            showText(""+timer, 720, 10);
+        } else
+        {
+            showText("",720,10);
+        }
+        if(timer < 1000)
+        {
+            showText(""+timer, 725, 10);
+        }
+        if(timer < 100)
+        {
+            showText("1", 725, 10);
+        }
         if (timer <= 0) {
             Greenfoot.stop();
+        }
+        if(WorldWidth == 200)
+        {
         }
     }
 
